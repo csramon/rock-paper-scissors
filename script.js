7113e2e2642e367b4e4842608247b108a5fb9ccb
csramon <89711947+csramon@users.noreply.github.com>
2022-01-26 19:11:20 -0300

    Format code

--- script.js
+++ script.js
@@ -7,69 +7,64 @@ function computerPlay() {
 let playerScore = 0;
 let computerScore = 0;
 
-function game() {
-  
-  for (i=0; i<5; i++) {
-  function playRound(playerSelection, computerSelection) {
+function game() {  
+    for (i=0; i<5; i++) {
+        function playRound(playerSelection, computerSelection) {
+            if (playerSelection === computerSelection) {
+                alert(`
+                Your choice: ${playerSelection}
+                Computer's choice: ${computerSelection}
+                It\'s a tie!
+                `)
+            }
+            if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+                alert(`
+                Your choice: ${playerSelection}
+                Computer's choice: ${computerSelection}
 
-  if (playerSelection === computerSelection) {
-    alert(`
-    Your choice: ${playerSelection}
-    Computer's choice: ${computerSelection}
-    It\'s a tie!
-    `)
-  }
+                ${playerSelection} beats ${computerSelection}
 
-  if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-    alert(`
-    Your choice: ${playerSelection}
-    Computer's choice: ${computerSelection}
+                You won this round!
+                `);
+                playerScore += 1;
+            }
+            if (playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "rock" && computerSelection === "paper") {
+                alert(`
+                Your choice: ${playerSelection}
+                Computer's choice: ${computerSelection}
 
-    ${playerSelection} beats ${computerSelection}
-
-    You won this round!
-    `);
-  playerScore += 1;
-  }
-
-  if (playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "rock" && computerSelection === "paper") {
-    alert(`
-    Your choice: ${playerSelection}
-    Computer's choice: ${computerSelection}
-
-    ${computerSelection} beats ${playerSelection}
+                ${computerSelection} beats ${playerSelection}
     
-    You lost this round!
-    `);
-  computerScore += 1;
-  }
+                You lost this round!
+                `);
+                computerScore += 1;
+            }
   }
   
   let playerSelection = prompt('Rock, paper, or scissors?') 
 
   let computerSelection = computerPlay(); 
-  //playerSelection and computerSelection need to be outside of the playRound function so they can be used as an argument
+  //playerSelection and computerSelection needed to be outside of the playRound function in order to be used as an argument
 
   console.log(playRound(playerSelection, computerSelection)); //playRound needed two parameters
 
 }
 
 if (playerScore>computerScore) {
-  alert(`
-FINAL SCORE
-Your score: ${playerScore}
-Computer's score: ${computerScore}
-You won!`);
-  } else if (playerScore<computerScore){
     alert(`
-    FINAL SCORE
-    Your score: ${playerScore}
-    Computer's score: ${computerScore}
-    You lost.`);
-  }
-  else {
-    alert('It\'s a tie! Press F5 and try again');
-  }
+        FINAL SCORE
+        Your score: ${playerScore}
+        Computer's score: ${computerScore}
+        You won!`);
+        } else if (playerScore<computerScore){
+            alert(`
+            FINAL SCORE
+            Your score: ${playerScore}
+            Computer's score: ${computerScore}
+            You lost.`);
+        } else {
+            alert('It\'s a tie! Press F5 and try again');
+        }
 }
   
 game();
