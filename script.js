9103f1daf7a36e0d7060c1ec99e6f3893a13b9bb
Ramon <ramonmateuscst@gmail.com>
2022-01-26 18:47:44 -0300

    Update script

--- script.js
+++ script.js
@@ -3,24 +3,73 @@ function computerPlay() {
     let randomPlay = Math.floor(Math.random() * choice.length);
     return choice[randomPlay]
   }
-const computerSelection = computerPlay();
-function playRound() {
-    let playerSelection = prompt('Rock, paper, or scissors?');
-    let availableOptions = ['rock', 'paper', 'scissors'];
-    if (playerSelection == computerSelection) {
-      alert(`Tie! You chose ${playerSelection} and computer chose ${computerSelection} as well.`)
-    } else if (playerSelection == 'rock' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'rock') {
-      alert(`
-      Your choice: ${playerSelection}
-      Computer's choice: ${computerSelection}
-      You lose! ${computerSelection} beats ${playerSelection}.`);
-    } else if (playerSelection != availableOptions) {
-      alert ('Invalid option!') //Check why does this appear even if it's an available option
-    } else {
-      alert (`
-      Your choice: ${playerSelection}
-      Computer's choice: ${computerSelection}
-      You won! ${playerSelection} beats ${computerSelection}.`)
-    } 
+
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+  
+  for (i=0; i<5; i++) {
+  function playRound(playerSelection, computerSelection) {
+
+  if (playerSelection === computerSelection) {
+    alert(`
+    Your choice: ${playerSelection}
+    Computer's choice: ${computerSelection}
+    It\'s a tie!
+    `)
   }
-  playRound();
+
+  if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+    alert(`
+    Your choice: ${playerSelection}
+    Computer's choice: ${computerSelection}
+
+    ${playerSelection} beats ${computerSelection}
+
+    You won this round!
+    `);
+  playerScore += 1;
+  }
+
+  if (playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "rock" && computerSelection === "paper") {
+    alert(`
+    Your choice: ${playerSelection}
+    Computer's choice: ${computerSelection}
+
+    ${computerSelection} beats ${playerSelection}
+    
+    You lost this round!
+    `);
+  computerScore += 1;
+  }
+  }
+  
+  let playerSelection = prompt('Rock, paper, or scissors?') 
+
+  let computerSelection = computerPlay(); 
+  //playerSelection and computerSelection need to be outside of the playRound function so they can be used as an argument
+
+  console.log(playRound(playerSelection, computerSelection)); //playRound needed two parameters
+
+}
+
+if (playerScore>computerScore) {
+  alert(`
+FINAL SCORE
+Your score: ${playerScore}
+Computer's score: ${computerScore}
+You won!`);
+  } else if (playerScore<computerScore){
+    alert(`
+    FINAL SCORE
+    Your score: ${playerScore}
+    Computer's score: ${computerScore}
+    You lost.`);
+  }
+  else {
+    alert('It\'s a tie! Press F5 and try again');
+  }
+}
+  
+game();
